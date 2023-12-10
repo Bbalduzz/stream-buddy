756642d883582741a3e9149a20a17bd74f2e060b
Bbalduzz <edoardoba2004@gmail.com>
2023-12-10 12:04:32 +0100

    fix typo in versioning control

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ def versioning_control(file_path="config.ini"):
     config.read(file_path)
     current_installed_version = config['VERSION']['local']
 
-    newest_version = re.search(r"local = (\d+\.\d+\.\d+)", requests.get("https://raw.githubusercontent.com/Bbalduzz/stream-buddy/main/config.ini").text)[0]
+    newest_version = re.search(r"local = (\d+\.\d+\.\d+)", requests.get("https://raw.githubusercontent.com/Bbalduzz/stream-buddy/main/config.ini").text)[0].removeprefix("local = ")
     if current_installed_version != newest_version:
         print(f"New version available: {newest_version}")
         return False
